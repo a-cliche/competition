4192960f0b71f4e2d72e286c091043b2b74ae666
Gabriela <a.cliche@abv.bg>
2016-05-18 20:39:21 +0300

    Main functionalities

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,6 @@
-$(function(){	
+$(function(){
+	var groups = []; //Array of objects(pairs of groups)
+	var allTeams = []; // All teams array
 
 	$.getJSON('assets/data/teams.json', function(res){
 
@@ -32,44 +34,134 @@ $(function(){
 				$('.groupB').append($(temp));
 				groupB.push(item);
 			}
-		});	
+		});
 
-		var rounds = 1;
+		var groupPair = [];
+		groupPair.push(groupA);
+		groupPair.push(groupB);
+		groups.push(groupPair);
+
+		var rounds = 2;
+		var matchesCount = teams.length/2.0;
 
 		for(var i=0;i<rounds;i++){
-			playRound(teams,groupA,groupB);
+			if (i !== 0) {
+				allTeams = uniteGroups(groups);
+
+				var newGroups = getNewGroups(allTeams);
+				// console.log('~~~~~~~');
+				// console.log('newGroups', newGroups);
+				// console.log('~~~~~~~');
+				// console.log('newGroups', newGroups);
+				generateTwoTeamsForEachGroup(newGroups);
+			} else {
+				playRound(matchesCount,groupA,groupB);
+			}
 		}
-	  
-
-
 	}
 
-	function playRound(teams,A,B){
+	function playRound(matchesCount,A,B){
 
-		var matches = teams.length/2.0;
-
-		for(var i=0;i<matches;i++){
+		for(var i=0;i<matchesCount;i++){
 			fight(A[i], B[i]);
 		}
-
 	}
 
 	function fight(home,away){
 		var result = Math.round(Math.random()*10)/10;
 
-		console.log(result)
-		if(result<0.5){
-			home.score ++;
+		if (!away) {
+			home.score += 1;
+		} else {
+			if(result<0.5){
+				home.score ++;
+			}
+			else if(result>0.5){
+				away.score ++;
+			}
+			else{
+				home.score += 0.5;
+				away.score += 0.5;
+			}
 		}
-		else if(result>0.5){
-			away.score ++;
+
+		// console.log('home', home);
+		// console.log('away', away);
+		// console.log('-------');
+	}
+
+	function uniteGroups(groups) {
+		var teams = [];
+
+		groups.forEach(function(pair, i) {
+			pair.forEach(function(group, i) {
+				teams.push(group);
+			});
+		});
+
+		teams = [].concat.apply([], teams);
+
+		teams.sort(function (a, b) {
+		  return b.score - a.score;
+		});
+
+		return teams;
+	}
+
+	function getNewGroups(teamsArray) {
+		var uniqueScores = [];
+		var newGroups = [];
+
+		for (i = 0; i < teamsArray.length; i++){
+	    if(uniqueScores.indexOf(teamsArray[i].score) === -1){
+	        uniqueScores.push(teamsArray[i].score);
+	    }
 		}
-		else{
-			home.score += 0.5;
-			away.score += 0.5;
+
+		for (i = 0; i < uniqueScores.length; i++) {
+			newGroups[i] = [];
+
+			newGroups[i] = teamsArray.filter(function(item) {
+				return item.score === uniqueScores[i];
+			});
+		}
+
+		for (i = 0; i < newGroups.length; i++) {
+			newGroups[i] = newGroups[i].sort(function(a, b) {
+				return a.id - b.id;
+			});
+		}
+
+		return newGroups;
+	}
+
+	function generateTwoTeamsForEachGroup(groupsArray) {
+		var groupCount = groupsArray.length;
+
+		for (i = 0; i < groupCount; i++) {
+			var groupA, groupB;
+			var currentGroupLength = 0;
+			var matchesCount = 0;
+
+			if (groupsArray[i].length % 2 !== 0 && i < groupCount - 1) {
+				var lastTeam = groupsArray[i].pop();
+				groupsArray[i+1].splice(0, 1, lastTeam);
+			}
+
+			currentGroupLength = groupsArray[i].length;
+			groupB = groupsArray[i];
+			groupA = groupsArray[i].splice(0, Math.ceil(currentGroupLength / 2.0));
+
+			matchesCount = currentGroupLength / 2.0;
+
+			console.log('matchesCount', matchesCount);
+			console.log('team ', i);
+			console.log('groupA ', groupA);
+			console.log('groupB ', groupB);
+			playRound(matchesCount,groupA,groupB);
+			// console.log('currentGroupLength / 2.0', Math.ceil(currentGroupLength / 2.0));
+
 		}
 	}
 
 });
-
-
