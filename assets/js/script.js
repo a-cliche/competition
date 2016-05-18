aaf44862d06ccc91dd24375bf48552665babd48f
danny <d.markov94@gmail.com>
2016-05-18 23:48:32 +0300

    Recursion

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,24 +14,17 @@ $(function(){
 
 	function generateTables(teams){
 
-		var allTeamList = $('.allTeams'),
-			groupA = [],
-			groupB = [],
-			temp;
+		var	groupA = [],
+			groupB = [];
 
 		teams.forEach(function (item,i){
-			temp = '<li><p> ID:' + item.id + ', Name: ' + item.name + '</p></li>';
-
-			allTeamList.append($(temp));
 
 			item.score = 0;
 
 			if(i<teams.length/2.0){
-				$('.groupA').append($(temp));
 				groupA.push(item);
 			}
 			else{
-				$('.groupB').append($(temp));
 				groupB.push(item);
 			}
 		});
@@ -39,32 +32,177 @@ $(function(){
 		var groupPair = [];
 		groupPair.push(groupA);
 		groupPair.push(groupB);
+
 		groups.push(groupPair);
 
-		var rounds = 2;
-		var matchesCount = teams.length/2.0;
+		var rounds = 1;
+		// var matchesCount = teams.length/2.0;
 
 		for(var i=0;i<rounds;i++){
-			if (i !== 0) {
-				allTeams = uniteGroups(groups);
 
-				var newGroups = getNewGroups(allTeams);
-				// console.log('~~~~~~~');
-				// console.log('newGroups', newGroups);
-				// console.log('~~~~~~~');
-				// console.log('newGroups', newGroups);
-				generateTwoTeamsForEachGroup(newGroups);
-			} else {
-				playRound(matchesCount,groupA,groupB);
+			if( i !== 0) {
+				groups = makeGroupPairs(allTeams);
 			}
+
+			allTeams = playRound(groups); // Todo: kak da raboti s for
+
+			// if (i !== 0) {
+			// 	allTeams = uniteGroups(groups);
+
+			// 	var newGroups = getNewGroups(allTeams);
+			// 	// console.log('~~~~~~~');
+			// 	// console.log('newGroups', newGroups);
+			// 	// console.log('~~~~~~~');
+			// 	// console.log('newGroups', newGroups);
+			// 	generateTwoTeamsForEachGroup(newGroups);
+			// } else {
+			// 	playRound(matchesCount,groupA,groupB);
+			// }
 		}
+
+		render(allTeams);
 	}
 
-	function playRound(matchesCount,A,B){
 
-		for(var i=0;i<matchesCount;i++){
-			fight(A[i], B[i]);
+
+	function playRound(groups){
+		var teams = [],
+			newGroupsTemp = [],
+			groupesWithEqScore = [];
+
+		playMatches(groups);
+
+		teams = uniteGroups(groups);
+
+		if(getGroupsWithEqualScore(teams)){
+			newGroupsTemp = getGroupsWithEqualScore(teams);
+
+			teams = removeRepeatingTeams(teams,newGroupsTemp);
+
+			groupesWithEqScore = makeGroupPairs(newGroupsTemp);
+
+			console.log(groupesWithEqScore);
+
+			// Recursion
+			// teams.concat(playRound(groupesWithEqScore));	
 		}
+
+
+		return teams;
+	}
+
+
+
+	// Removes repeating items from two arrays
+	function removeRepeatingTeams(removeFrom,toRemove){
+
+		toRemove = flattenArray(toRemove); 
+
+		for (i = 0; i < removeFrom.length; i++){
+			for(j = 0; j < toRemove.length; j++){
+			    if(removeFrom[i].id === toRemove[j].id){
+			        removeFrom.splice(i, 1);
+			    }	
+			}
+		}
+
+		return removeFrom;
+	}
+
+
+	// Splits groups in two
+	function makeGroupPairs(groupsArray) {
+		var groupCount = groupsArray.length,
+			result = [];
+
+		for (i = 0; i < groupCount; i++) {
+			var groupA, groupB;
+			var currentGroupLength = 0;
+
+			if (groupsArray[i].length % 2 !== 0 && i < groupCount - 1) {
+				var lastTeam = groupsArray[i].pop();
+				groupsArray[i+1].splice(0, 1, lastTeam);
+			}
+
+			currentGroupLength = groupsArray[i].length;
+			groupB = groupsArray[i];
+			groupA = groupsArray[i].splice(0, Math.ceil(currentGroupLength / 2.0));
+
+			result.push([groupA,groupB]);
+		}
+
+		return result;
+	}
+
+
+
+	function getGroupsWithEqualScore(teamsArray) {
+		var uniqueScores = [];
+		var newGroups = [];
+
+		for (i = 0; i < teamsArray.length; i++){
+		    if(uniqueScores.indexOf(teamsArray[i].score) === -1){
+		        uniqueScores.push(teamsArray[i].score);
+		    }
+		}
+
+		for (i = 0; i < uniqueScores.length; i++) {
+			newGroups[i] = [];
+
+			newGroups[i] = teamsArray.filter(function(item) {
+				return item.score === uniqueScores[i];
+			});
+		}
+
+		for (i = 0; i < newGroups.length; i++) {
+			newGroups[i] = newGroups[i].sort(function(a, b) {
+				return a.id - b.id;
+			});
+		}
+
+		return newGroups;
+	}
+
+
+
+
+	function uniteGroups(groups) {
+		var teams = [];
+
+		groups.forEach(function(pair, i) {
+			pair.forEach(function(group, i) {
+				teams.push(group);
+			});
+		});
+
+		teams = flattenArray(teams);
+
+		teams.sort(function (a, b) {
+		  return b.score - a.score;
+		});
+
+		return teams;
+	}
+
+
+
+
+	function playMatches(groups){
+
+		var A,B,
+			matchesCount = 0;
+
+		groups.forEach(function(pair){
+
+			A = pair[0];
+			B = pair[1];
+			matchesCount = A.length;
+
+			for(var i=0;i<matchesCount;i++){
+				fight(A[i], B[i]);
+			}
+
+		});
 	}
 
 	function fight(home,away){
@@ -90,78 +228,24 @@ $(function(){
 		// console.log('-------');
 	}
 
-	function uniteGroups(groups) {
-		var teams = [];
 
-		groups.forEach(function(pair, i) {
-			pair.forEach(function(group, i) {
-				teams.push(group);
-			});
+
+
+	function render(teams){
+		var allTeamList = $('.allTeams'),
+			html = '';
+
+		teams.forEach(function (item,i){
+
+			html = '<li><p> ID:' + item.id + ', Name: ' + item.name + ', Score: ' + item.score + '</p></li>';
+
+			allTeamList.append($(html));
 		});
-
-		teams = [].concat.apply([], teams);
-
-		teams.sort(function (a, b) {
-		  return b.score - a.score;
-		});
-
-		return teams;
 	}
 
-	function getNewGroups(teamsArray) {
-		var uniqueScores = [];
-		var newGroups = [];
-
-		for (i = 0; i < teamsArray.length; i++){
-	    if(uniqueScores.indexOf(teamsArray[i].score) === -1){
-	        uniqueScores.push(teamsArray[i].score);
-	    }
-		}
-
-		for (i = 0; i < uniqueScores.length; i++) {
-			newGroups[i] = [];
-
-			newGroups[i] = teamsArray.filter(function(item) {
-				return item.score === uniqueScores[i];
-			});
-		}
-
-		for (i = 0; i < newGroups.length; i++) {
-			newGroups[i] = newGroups[i].sort(function(a, b) {
-				return a.id - b.id;
-			});
-		}
-
-		return newGroups;
+	function flattenArray(arr){
+		return [].concat.apply([], arr);
 	}
 
-	function generateTwoTeamsForEachGroup(groupsArray) {
-		var groupCount = groupsArray.length;
-
-		for (i = 0; i < groupCount; i++) {
-			var groupA, groupB;
-			var currentGroupLength = 0;
-			var matchesCount = 0;
-
-			if (groupsArray[i].length % 2 !== 0 && i < groupCount - 1) {
-				var lastTeam = groupsArray[i].pop();
-				groupsArray[i+1].splice(0, 1, lastTeam);
-			}
-
-			currentGroupLength = groupsArray[i].length;
-			groupB = groupsArray[i];
-			groupA = groupsArray[i].splice(0, Math.ceil(currentGroupLength / 2.0));
-
-			matchesCount = currentGroupLength / 2.0;
-
-			console.log('matchesCount', matchesCount);
-			console.log('team ', i);
-			console.log('groupA ', groupA);
-			console.log('groupB ', groupB);
-			playRound(matchesCount,groupA,groupB);
-			// console.log('currentGroupLength / 2.0', Math.ceil(currentGroupLength / 2.0));
-
-		}
-	}
 
 });
